44fe275f7fa4e9719284184bb24e36a03aee219b
Akmal-M <akmalutube77@gmail.com>
2020-12-22 02:47:05 +0500

    added features

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 const Header = () => {
     return (
         <div className='sidebar'>
-            <Link to="/online">Online</Link>
+            <a href="https://us05web.zoom.us/j/9277961791?pwd=YXRFUGMrOWtRcjhOYTZ5V2RmZlBpdz09" target="_blank">Online</a>
             <a href='https://t.me/joinchat/MTUE8xndl7pJ8XLGnJnEMg' target="_blank">Chat</a>
             <Link to="/testimonial">Fikrlar</Link>
             <Link to="/contact">Manzil</Link>
