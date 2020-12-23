e251331f8df92cdc15006d0c2b518ea6bf2933e2
Akmal-M <akmalutube77@gmail.com>
2020-12-23 23:04:18 +0500

    added some changes

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export default {
             _id: 1,
         img:'https://images.unsplash.com/photo-1432656878500-70bbc0c37e85?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzc3fHxwZXJzb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
         name: 'morgan',
-        rank: 'onasi',
+        rank: 'Adasi',
         text: '"I have an awesome idea for a startup, i ' +
             'need you to build it for me theres all this ' +
             'spanish text on my site so i "'
@@ -14,14 +14,14 @@ export default {
             _id: 2,
             img:'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
             name: 'morgan',
-            rank: 'onasi',
+            rank: 'Onasi',
             text: '"the same or better remember, everything is the same or ' +
                 'better I like it, but can the snow look a little warmer"'
         }, {
             _id: 3,
             img:'https://images.unsplash.com/photo-1596305589393-cc3bd367d13a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzU2fHxwZXJzb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
             name: 'morgan',
-            rank: 'adasi',
+            rank: 'Onasi',
             text: '"One morning, when Gregor Samsa woke from troubled dreams,' +
                 ' he found' +
                 ' himself transformed in his bed into a ." '
