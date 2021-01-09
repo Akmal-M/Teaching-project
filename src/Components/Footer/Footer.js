f5f6a0b4f68e1435e1c34512cc733b8829d96f9c
Akmal-M <akmalutube77@gmail.com>
2021-01-09 19:03:20 +0500

    deploy

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import {FaFacebook, FaInstagram, FaTelegram} from "react-icons/all";
+import {FaFacebook, FaInstagram, FaTelegram} from "react-icons/fa";
 
 
 const Footer = () => {
